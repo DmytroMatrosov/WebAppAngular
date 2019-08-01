c5d4b1bbe6a6ad153bca2e10a8aa5016588aac65
Dmytro Matrosov <dmytro.matrosov21@gmail.com>
2019-08-01 12:36:07 +0300

    in process of optimising code of classList

--- src/app/classlist/classlist.component.ts
+++ src/app/classlist/classlist.component.ts
@@ -14,41 +14,68 @@ export class ClassListComponent implements OnInit {
   ) { }
   private endPoint: string = environment.APIEndpoint;
   localStorageToken: string = localStorage.getItem('token');
-
+  
   displayedColumns: string[] = [
     'className',
     'classYear',
     'numOfStudents'
   ];
-  dataSource: ClassTable[];
-  activeClassList: ClassTable[];
-  nonActiveClassList: ClassTable[];
   // declair the obj  activeUniqueClasses
-  activeUniqueClasses = {};
-  nonActiveUniqueClassList: ClassTable[];
+  activeUniqueClassList = new Map();
+  nonActiveUniqueClassList = new Map();
+  // nonActiveUniqueClassList = {
+  //   "uniqueGroupofClass": ClassTable[]
+  // };
 
+  // Получить данные с классами с сервера.
+  // Отфильтровать их по статусу.
+  // Поместить в зависимости от статуса в мэп с ключем уникальный клас и значением массивом классов.
+
+
+  getClasses(){
+    this.http.get(
+      `${this.endPoint}classes`,
+      { headers: { Authorization: this.localStorageToken } }
+    ).subscribe(
+      response => { 
+        //response['data']
+        this.groupByStatus(response['data']);
+      })      
+  }
+  groupByStatus(classList){
+    classList.forEach(function(schoolClass){
+      debugger
+      const uniqueClassName = this.getUniqueClassName(schoolClass); 
+      if(schoolClass.isActive){
+        this.activeUniqueClassList.has(uniqueClassName) ?
+        this.activeUniqueClassList.get(uniqueClassName).push(schoolClass) :
+        this.activeUniqueClassList.set(uniqueClassName,[schoolClass])
+      }
+      else{
+        this.nonActiveUniqueClassList.has(uniqueClassName) ?
+        this.nonActiveUniqueClassList.get(uniqueClassName).push(schoolClass) :
+        this.nonActiveUniqueClassList.set(uniqueClassName,[schoolClass])
+      }
+    })
+  }
+
+  getUniqueClassNames(schoolClass){
+    let indexOfDash = schoolClass['className'].indexOf('-');
+    if(schoolClass['className'].includes('(')){
+      return schoolClass['className'].substring(0,indexOfDash) + ")"
+    }
+    else{
+      return schoolClass['className'].substring(0,indexOfDash)
+    }
+  }
   ngOnInit() {
     // turn on httpService.
     // send get req. to endpoint to recieve classlist.
     // add to this req. url params 'Authorization' 
     // after recieving response, to assign recieved data to dataSource (this.dataSource)
-    this.http.get(
-      `${this.endPoint}classes`,
-      { headers: { Authorization: this.localStorageToken } }
-    ).subscribe(
-      response => {
-        this.dataSource = response['data'] as ClassTable[];
-        // take only "active" objects from arr (isActive: true/false)
-        this.activeClassList = this.dataSource.filter(activeClass => activeClass.isActive === true);
-        this.nonActiveClassList = this.dataSource.filter(nonActiveClass => nonActiveClass.isActive === false);
-        
-          for (var oneClass of this.activeClassList) {
-          var indexOfDash = oneClass['className'].indexOf('-'); 
-          var uniqueClass = oneClass['className'].includes('(') ? oneClass['className'].substring(0,indexOfDash) + ')' : oneClass['className'].substring(0,indexOfDash);
-          this.activeUniqueClasses[uniqueClass] ? this.activeUniqueClasses[uniqueClass].push(oneClass): this.activeUniqueClasses[uniqueClass] = [oneClass]                   
-        }
-      })
-      console.log(this.activeUniqueClasses);
+    this.getClasses()
+    console.log(this.activeUniqueClassList);
+    console.log(this.nonActiveUniqueClassList);
   }
   // +++take only "active" objects from arr (isActive: true/false)
   // +++declair the obj  activeUniqueClasses
@@ -58,7 +85,6 @@ export class ClassListComponent implements OnInit {
   // create accordions with all keys from activeUniqueClasses obj in active classes accordion
   // put to each accordion value / classlist, according to name of accordion/key of obj
 }
-
 
 export interface ClassTable {
   className: string;
